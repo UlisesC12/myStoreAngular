cbff04c9881b7d91dfa88d175132036ef30b5e76
UlisesC12 <ulisescandela99@gmail.com>
2023-01-04 01:17:35 -0600

    saved changes

---
+++ src/app/recap.ts
@@ -0,0 +1,18 @@
+//TYPESCRIPT
+//Se puede trabajar en tipados tanto en funciones como en variables
+const username: string = "ulisescandela";
+const sum = (a: number, b: number) => {
+return a+b;
+}
+//Reduce errores, ya que se deben recibir los tipos de variables previamente configurados
+sum(1,3);
+
+//Es un lenguaje orientado a objetos
+class Person{
+    constructor(private age: number, public lastName: string){}
+}
+
+const nico = new Person(15, "Molina");
+//Se pueden tener variables privadas:
+nico.age;
+
